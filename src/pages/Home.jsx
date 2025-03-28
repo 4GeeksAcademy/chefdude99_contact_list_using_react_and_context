5da004b23122ffd00d94d5a861aebd7c82be53fb
chefdude99 <chefdude.1999@gmail.com>
2025-03-28 23:22:54 +0000

    Listo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { json, useNavigate } from "react-router-dom";
 import CardContact from "../components/CardContact.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { useEffect } from "react";
@@ -8,8 +8,18 @@ export const Home = () => {
 	const { store, dispatch } = useGlobalReducer()
 	useEffect(() => {
 		fetch("https://playground.4geeks.com/contact/agendas/chefdude99/contacts")
-			.then((response) => response.json())
-			.then((data) =>  dispatch({ type: "set_contactos", payload: data.contacts }))
+			.then((response) => {
+				if (response.status === 404) {
+					fetch("https://playground.4geeks.com/contact/agendas/chefdude99", {
+						method: "POST",
+					})
+						.then((response) => {
+							console.log(response)
+						})
+				}
+				return (response.json())
+			})
+			.then((data) => dispatch({ type: "set_contactos", payload: data.contacts }))
 			.catch((error) => console.log(error))
 	}, [])
 
@@ -17,10 +27,10 @@ export const Home = () => {
 
 	return (
 		<div className="container text-center mt-5">
-			{
-				store.contactos.map((contact, index) => {
+			{store.contactos.length === 0 ? <h1>Ingresa un Nuevo Contacto</h1> :
+				store.contactos?.map((contact, index) => {
 					return (
-						<CardContact contact={contact} key={index}/>
+						<CardContact contact={contact} key={index} />
 					)
 				})
 			}
