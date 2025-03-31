98983485a0c6256a0529ca6c26b2bfd9ba2031c0
chefdude99 <chefdude.1999@gmail.com>
2025-03-31 18:06:32 +0000

    aun falta

--- src/components/CardContact.jsx
+++ src/components/CardContact.jsx
@@ -25,13 +25,14 @@ const CardContact = ({ contact }) => {
                         if (!response.ok) {
                             throw new Error("error al modificar el contacto")
                         }
-                        navigate("/")
+                        return response.json();
                     })
                     .then(() => {
-                        window.location.href = '/'
+                        window.location.replace = ("/")
                     })
                     .catch((error) => {
-                        return error
+                        console.error("Error:", error);
+                        Swal.fire("Error", "No se pudo eliminar el contacto.", "error");
                     })
             }
         });
@@ -62,28 +63,12 @@ const CardContact = ({ contact }) => {
                             onClick={() => handleDelete(contact.id)}>
                             <i className="fa-solid fa-trash-can"></i>
                         </button>
-                        <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div className="modal-dialog">
-                                <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h1 className="modal-title fs-5" id="exampleModalLabel">Estas seguro ?</h1>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div className="modal-body">
-                                        Si borras este contacto no podras recuperarlo jamas ni nunca !!!
-                                    </div>
-                                    <div className="modal-footer">
-                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                        <button type="button" className="btn btn-primary"
-                                            onClick={() => handleDelete(contact.id)}>Eliminar</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
+          
     )
 }
 
